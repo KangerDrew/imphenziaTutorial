e676faa3b2b631665058cca3dca189d5d5c137a8
Drew Kang <85123383+KangerDrew@users.noreply.github.com>
2022-10-10 03:01:47 -0700

    Need to review layers and collisions

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,10 @@ public class Player : MonoBehaviour
     // This will retrive the transform (rotation, position, scale) of a
     // specified model in the game:
     [SerializeField] private Transform groundCheckTransform = null;
+    // ???:
+    [SerializeField] private LayerMask playerMask;
+
+    // Unexposed fields:
     private bool jumpKeyWasPressed;
     private float horizontalInput;
     private Rigidbody rigidbodyComponent;
@@ -14,7 +18,10 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // Get the rigidbody component of the player model:
+        // Get the rigidbody component of the player model. This is so
+        // that everytime we need to do something with Rigidbody, we
+        // don't call it over and over again and needlessly making unity
+        // look for the reference. We instead call it just once:
         rigidbodyComponent = GetComponent<Rigidbody>();
     }
 
@@ -35,9 +42,16 @@ public class Player : MonoBehaviour
     {
         // Generate a sphere at transform's position (of size 0.1), and see how many colliders it
         // overlaps. When player model is in the air, there should be only 1 (groundCheckTransform
-        // is colliding with the player transform). When player is on the ground .Length value should
+        // is "colliding" with the player transform). When player is on the ground .Length value should
         // be 2 since it's also colliding with the floor:
-        if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f).Length == 1)
+        //if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f).Length == 1)
+        //{
+        //    return;
+        //}
+
+        // Alternate method for preventing player from jumping midair - Use LayerMask
+        // collision detection:
+        if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f, playerMask).Length == 0)
         {
             return;
         }
