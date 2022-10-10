372aef045fc12974700af576ff699c8f5d6748ea
Drew Kang <85123383+KangerDrew@users.noreply.github.com>
2022-10-10 02:29:44 -0700

    added logic for player model jumping

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
+    // This will retrive the transform (rotation, position, scale) of a
+    // specified model in the game:
+    [SerializeField] private Transform groundCheckTransform = null;
     private bool jumpKeyWasPressed;
+    private float horizontalInput;
+    private Rigidbody rigidbodyComponent;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Get the rigidbody component of the player model:
+        rigidbodyComponent = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -21,15 +26,33 @@ public class Player : MonoBehaviour
         {
             jumpKeyWasPressed = true;
         }
+
+        horizontalInput = Input.GetAxis("Horizontal");
     }
 
     // FixedUpdate is called once every physics update:
     private void FixedUpdate()
     {
+        // Generate a sphere at transform's position (of size 0.1), and see how many colliders it
+        // overlaps. When player model is in the air, there should be only 1 (groundCheckTransform
+        // is colliding with the player transform). When player is on the ground .Length value should
+        // be 2 since it's also colliding with the floor:
+        if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f).Length == 1)
+        {
+            return;
+        }
+
+        // Applies jump motion to Player model, then re-sets jump key
+        // boolean to false (so it doesn't fly away):
         if (jumpKeyWasPressed)
         {
-            GetComponent<Rigidbody>().AddForce(Vector3.up * 5, ForceMode.Impulse);
+            rigidbodyComponent.AddForce(Vector3.up * 5, ForceMode.Impulse);
             jumpKeyWasPressed = false;
         }
+
+        // Takes the horizontalInput float value, and apply it to the model's Vector3.x:
+        rigidbodyComponent.velocity = new Vector3(horizontalInput, rigidbodyComponent.velocity.y, 0);
     }
+
+
 }
